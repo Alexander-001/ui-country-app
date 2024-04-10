7ebaa3f16c723c6bb44633982b86e126d02f8f22
Alexander Ortiz <CL198785156@enel.com>
2024-04-10 15:41:06 -0400

    first commit

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styles: [],
+})
+export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+
+  constructor(private countryService: CountriesService) {}
+
+  searchByCapital(term: string): void {
+    this.countryService.searchCapital(term).subscribe((resp) => {
+      this.countries = resp;
+    });
+  }
+}
